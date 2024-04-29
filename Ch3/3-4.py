00f2e432dd8507e170abfc75ccd544c3291e2e15
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-04-29 16:32:46 +0330

    3-4 Done.

---
+++ Ch3/3-4.py
@@ -0,0 +1,12 @@
+"""
+    3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+"""
+    
+guests_list = [ 'Seneca', 'Marcus Aurelius', 'Xeno', 'Emerson', 'David Goggins' ]
+
+print()
+
+for guest in guests_list:
+    print(f"{guest.title()} is invited to the party.")
+
+print()
