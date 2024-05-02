1727ed06f06f8101db8a4a7f80688c31fdefcd07
Amir-Behzad <a.behzad.ebr@gmail.com>
2024-05-02 11:37:40 +0330

    3-9.py Done.

---
+++ Ch3/3-9.py
@@ -0,0 +1,15 @@
+"""
+ 3.9 Dinner Guests: Working with one of the programs from Exercises 3-4 through 3-7 (pages 41–42), use len() to print a message indicating the number of people you’re inviting to dinner.
+"""
+guests_list = [
+    'Seneca',
+    'Marcus Aurelius',
+    'Zeno',
+    'Emerson',
+    'David Goggins'
+    ]
+
+print(
+    'Number of guests:',
+    len(guests_list)
+)
